53e7e88c44406208e38b9001920ed5a007077b9d
yassercf <l1nk.darck@gmail.com>
2015-05-22 15:32:49 -0400

    Se montó la plantilla base y de portada; traducción al español, cambio del video fondo, plantilla del formulario de incidencias

--- src/Segurex/SegurexBundle/Controller/DefaultController.php
+++ src/Segurex/SegurexBundle/Controller/DefaultController.php
@@ -7,7 +7,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     public function indexAction()
-    {
-        return $this->render('SegurexBundle:Default:index.html.twig');
+    {       
+        /*
+            $titulares: Contiene los 6 ultimos titulares marcados como ESPECIAL en la Base de Datos de articulos.
+        */
+        $titulares = array(
+            't1' => "Las bases estratégicas de la informatización cubana", 
+            't2' => "Vulnerabilidad en Drupal deja a millones de sitios web en peligro",
+            't3' => "GHOST: Grave vulnerabilidad en sistemas Linux",
+            't4' => "Circula por la Red Nacional el programa maligno Win32.Onion.",
+            't5' => "Vulnerabilidad SuperFish en portátiles Lenovo",
+            't6' => "Graves vulnerabilidades en WordPress",);
+        return $this->render('SegurexBundle:Default:index.html.twig', array('titulares' => $titulares));
     }
+
+    public function errorAction()
+    {    	    	   	
+        return $this->render('SegurexBundle:Default:404.html.twig');
+    }
+
+
 }
